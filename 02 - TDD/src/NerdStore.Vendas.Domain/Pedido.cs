b7ef3608b9296f374f4c894640f54f3fee2bce0a
Matheus <matheusnevesdecarvalho@gmail.com>
2020-12-16 10:41:55 -0300

    aulas

---
+++ 02 - TDD/src/NerdStore.Vendas.Domain/Pedido.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NerdStore.Vendas.Domain
+{
+    public class Pedido
+    {
+        public decimal ValorTotal { get; private set; }
+        public IList<PedidoItem> PedidoItems { get; set; } = new List<PedidoItem>();
+
+        public void AdicionarItem(PedidoItem pedidoItem)
+        {
+            PedidoItems.Add(pedidoItem);
+
+            ValorTotal = PedidoItems.Sum(i => i.ValorTotal());
+        }
+    }
+}
---
+++ 02 - TDD/tests/NerdStore.Vendas.Domain.Tests/PedidoTests.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace NerdStore.Vendas.Domain.Tests
+{
+    public class PedidoTests
+    {
+        [Fact(DisplayName = "Adicionar Item Novo Pedido")]
+        [Trait("Categoria", "Pedido Tests")]
+        public void AdicionarItemPedido_NovoPedido_DeveAtualizarValor()
+        {
+            // Arrange
+            var pedido = new Pedido();
+            var pedidoItem = new PedidoItem(Guid.NewGuid(), "Produto Teste", 2, 100);
+
+            // Act
+            pedido.AdicionarItem(pedidoItem);
+
+            // Assert
+            pedido.ValorTotal.Should().Be(200);
+        }
+    }
+}
